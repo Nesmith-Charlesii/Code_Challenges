a8369257652d9d1e71c721dbc6da27883294aa14
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-09 19:08:48 -0400

    created jumbledNumbers.js for jumbledNumbers algorithm. This algorithm takes a string of jumbled letters and returns integers corresponding to the numerals found in the jumbled string

---
+++ jsPlayground/jumbledNumbers.js
@@ -0,0 +1,61 @@
+// jumbled numbers 
+const jumbleNumber = (str) => {
+    let numerals = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let numberText = [];
+    let numbersFound = [];
+    let usedLetters = [];
+    for(let i = 0; i < numerals.length; i++) {
+        for(let j = 0; j < numerals[i].length; j++) {
+            if(str.includes(numerals[i][j])) {
+                // Splits every letter of input 'str' into individual elements within an array
+                let splitString = str.split("");
+                // Find the index of the included letter within the input 'str'
+                let stringIndex = splitString.indexOf(numerals[i][j]);
+                // Remove the included letter from the input 'str' so that it is only used once
+                let letter = splitString.splice(stringIndex, 1);
+                usedLetters.push(letter);
+            }
+        }
+    }
+    let joinLetters = usedLetters.join("");
+    for(let k = 0; k < numerals.length; k++) {
+        if(joinLetters.includes(numerals[k])) {
+            numberText.push(numerals[k]);
+            switch(numerals[k]) {
+                case "zero":
+                    numbersFound.push(0)
+                    break;
+                case "one":
+                    numbersFound.push(1)
+                    break;
+                case "two":
+                    numbersFound.push(2)
+                    break;
+                case "three":
+                    numbersFound.push(3)
+                    break;
+                case "four":
+                    numbersFound.push(4)
+                    break;
+                case "five":
+                    numbersFound.push(5)
+                    break;
+                case "six":
+                    numbersFound.push(6)
+                    break;
+                case "seven":
+                    numbersFound.push(7)
+                    break;
+                case "eight":
+                    numbersFound.push(8)
+                    break;
+                case "nine":
+                    numbersFound.push(9)
+                    break;
+            }
+        }
+    }
+    console.log(`Number Text Found: ${numberText.join(" ")}\nNumber Found: ${numbersFound.join("")}`);
+}
+
+jumbleNumber("reuonnoinfe")
