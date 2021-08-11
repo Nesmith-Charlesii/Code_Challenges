f23125791b82a4204fc8f960600a7d0ca9dab3bd
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-10 21:03:21 -0400

    Revised luckyNumbers algorithm with comments for better understanding

--- jsPlayground/luckyNumbers.js
+++ jsPlayground/luckyNumbers.js
@@ -1,9 +1,14 @@
+// This algorithm finds all lucky numbers between a low number and a high number containing either a 6 or an 8, but not both
+
 const luckyNumbers = (l, h) => {
     let count = 0;
     for(let i = l; i <= h; i++) {
         let currentValue = i;
+        // Convert integer to a string to split into individual items
         let currentString = currentValue.toString();
+        // Split string into individual items/ returns an array
         let splitString = currentString.split("");
+        // Check if either a 6 or an 8 are included in the array
         if(splitString.includes("6") && splitString.includes("8")){
             continue;
         } else if(splitString.includes("6") || splitString.includes("8")){
