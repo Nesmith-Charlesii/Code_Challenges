ba1bdade495ae0620cfb0f0ed08b08b9c69edcba
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-30 14:01:25 -0400

    altered original Kangaroo algorithm to only have if statements

--- jsPlayground/kangaroo.js
+++ jsPlayground/kangaroo.js
@@ -1,20 +1,14 @@
 "use strict";
 
 const kangaroo = (x1, v1, x2, v2) => {
-    let x1Pos = x1
-    let x2Pos = x2;
-    let jumpCount = 0;
-    for(let i = 1; i <= Infinity; i++) {
-        if(v2 > v1 && x2 > x1) {
-            return "NO";
+    if(v1 > v2) {
+        let remainder = (x1-x2) % (v2-v1);
+        if(remainder==0) {
+            return "YES";
         }
-        jumpCount = i;
-        x1Pos += v1;
-        x2Pos += v2; 
-        if(x1Pos === x2Pos) {
-            return `Yes @ ${jumpCount > 1 ? jumpCount += " jumps" : jumpCount += " jump"}`;
-        } 
-    }
+        return "NO";
+    } 
+    return "NO";
 }
 
-console.log(kangaroo(0, 3, 4, 2));
+console.log(kangaroo(0, 2, 5, 3));
